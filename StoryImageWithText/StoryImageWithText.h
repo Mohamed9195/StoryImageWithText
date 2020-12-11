0e714e5e3f2db8bb69a201ffcfa67d15e8330391
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-12-11 11:59:36 +0200

    Initial Commit

---
+++ StoryImageWithText/StoryImageWithText.h
@@ -0,0 +1,18 @@
+//
+//  StoryImageWithText.h
+//  StoryImageWithText
+//
+//  Created by mohamed hashem on 11/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StoryImageWithText.
+FOUNDATION_EXPORT double StoryImageWithTextVersionNumber;
+
+//! Project version string for StoryImageWithText.
+FOUNDATION_EXPORT const unsigned char StoryImageWithTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryImageWithText/PublicHeader.h>
+
+
